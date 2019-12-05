1f49486dfb62673f73939a82c46beca8b6ea2d34
Amit Gaikwad <amitgaikwad85@gmail.com>
2019-12-05 22:35:58 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import {render} from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let studentList = [
+    {"name": "Amit Gaikwad", "age": 16, "classroom": 10},
+    {"name": "Archana Gaikwad", "age": 18, "classroom": 11},
+    {"name": "Madan Gaikwad", "age": 30, "classroom": 15},
+    {"name": "Abhijit Gaikwad", "age": 35, "classroom": 20}
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Student = ({name, age, classroom}) => {
+    return (
+        <section>
+            <h2>Name: {name}</h2>
+            <p>Age: {age}</p>
+            <p>class: {classroom}</p>
+        </section>
+    )
+}
+
+const School = ({students}) => {
+    return (
+        <div>
+            {students.map(
+                (student, i) => <Student key={i} 
+                                name={student.name} 
+                                age={student.age} 
+                                classroom={student.classroom} />
+            )
+        }
+        </div>
+    )
+}
+
+// const Hello = ({fname, lname, msg}) => {
+//     return (
+//         <div>
+//             <h1>{msg}</h1>
+//             <p style={{fontWeight: "bold"}}>Author: {fname} {lname}</p>
+//         </div>
+//     )
+// }
+
+// const Hello = (props) => {
+//     return ( 
+//         <div>
+//             <h1>{props.msg}</h1>
+//             <p style={{fontWeight: "bold"}}>Author: {props.fname} {props.lname}</p>
+//         </div>
+//     )
+// }
+
+// class Hello extends Component {
+//     render() {
+//         const {fname, lname, msg} = this.props;
+//         return (
+//             <div>
+//                 <h1>{msg}</h1>
+//                 <p style={{fontWeight: "bold"}}>Author: {fname} {lname}</p>
+//             </div>
+//         )
+//     }
+// }
+
+render(<School students={studentList}/>, document.getElementById('root'))
